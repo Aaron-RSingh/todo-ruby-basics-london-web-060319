f81beac4ca3f474723a13f8697ae3959244197bb
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-25 17:10:43 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
-
+def return_a_value(phrase==nil)
+return phrase
 end
 
 def last_evaluated_value
